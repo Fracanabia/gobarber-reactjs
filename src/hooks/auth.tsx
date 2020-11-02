99001d4b44806404b00dd2b0e2dd174bcfb629e3
Wesley Fracanábia <wesleyf.santos92@hotmail.com>
2020-11-02 20:19:32 -0300

    rocketseat: gobarber

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,70 @@
+import React, { createContext, useCallback, useContext, useState } from 'react'
+import api from '../services/api'
+
+interface AuthState {
+  token: string
+  user: object
+}
+
+interface SignInCredentials {
+  email: string
+  password: string
+}
+
+interface AuthContextData {
+  user: object
+  signIn(credendials: SignInCredentials): Promise<void>
+  signOut(): void
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@goBarber:token')
+    const user = localStorage.getItem('@goBarber:user')
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) }
+    }
+
+    return {} as AuthState
+  })
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    })
+
+    const { user, token } = response.data
+    localStorage.setItem('@goBarber:token', token)
+    localStorage.setItem('@goBarber:user', JSON.stringify(user))
+
+    setData({ token, user })
+  }, [])
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@goBarber:token')
+    localStorage.removeItem('@goBarber:user')
+
+    setData({} as AuthState)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth mest be used within an AuthProvider')
+  }
+
+  return context
+}
+
+export { AuthProvider, useAuth }
